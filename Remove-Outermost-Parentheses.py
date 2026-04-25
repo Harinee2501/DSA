9c00364754da7fbd0879f170501eddf86875817c
Harinee Manikandan <152697438+Harinee2501@users.noreply.github.com>
2026-04-25 11:06:00 +0530

    [1021] [Time Beats: 18.62%] [Memory Beats: 95.57%] - LeetPush

--- Remove-Outermost-Parentheses.py
+++ Remove-Outermost-Parentheses.py
@@ -1,14 +1,14 @@
 1class Solution(object):
 2    def removeOuterParentheses(self, s):
-3        stack=[]
+3        stack=0
 4        decomposed=""
 5        for i in s:
 6            if i=='(':
-7                if len(stack)>0:
+7                if stack>0:
 8                    decomposed+=i
-9                stack.append(i)
+9                stack+=1
 10            else:
-11                stack.pop()
-12                if len(stack)>0:
+11                stack-=1
+12                if stack>0:
 13                    decomposed+=i
 14        return decomposed
