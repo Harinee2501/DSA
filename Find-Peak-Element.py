0f6a9c840fbac49ca92518d7f079b3a6e2a746a0
Harinee Manikandan <152697438+Harinee2501@users.noreply.github.com>
2026-05-02 11:55:47 +0530

    [162] [Time Beats: 100.00%] [Memory Beats: 16.93%] - LeetPush

--- Find-Peak-Element.py
+++ Find-Peak-Element.py
@@ -1,3 +1,12 @@
 1class Solution(object):
 2    def findPeakElement(self, nums):
-3        return nums.index(max(nums))
+3        l=0
+4        r=len(nums)-1
+5        while l<r:
+6            mid=(l+r)//2
+7            if nums[mid]<nums[mid+1]:
+8                l=mid+1
+9            else:
+10                r=mid
+11        return l
+12        
