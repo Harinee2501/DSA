a0bb296d5a14fb172c83c9d93ac02e22078f644a
Harinee Manikandan <152697438+Harinee2501@users.noreply.github.com>
2026-05-03 08:49:59 +0530

    [796] [Time Beats: 100.00%] [Memory Beats: 89.72%] - LeetPush

--- Rotate-String.py
+++ Rotate-String.py
@@ -1,15 +1,18 @@
 1class Solution(object):
 2    def rotateString(self, s, goal):
-3        n=len(s)
-4        length=n
-5        if s==goal:
-6            return True
-7        while n>0:
-8            temp=""
-9            for i in range(length):
-10                temp+=s[(i+1)%length]
-11            if temp==goal:
-12                return True
-13            s=temp
-14            n-=1
-15        return False
+3        if len(s)==len(goal) and goal in s+s:
+4            return True
+5        return False
+6        # n=len(s)
+7        # length=n
+8        # if s==goal:
+9        #     return True
+10        # while n>0:
+11        #     temp=""
+12        #     for i in range(length):
+13        #         temp+=s[(i+1)%length]
+14        #     if temp==goal:
+15        #         return True
+16        #     s=temp
+17        #     n-=1
+18        # return False
