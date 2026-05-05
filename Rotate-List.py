4a8740ba40f12a4417edf47dfff0cb726d31d2bb
Harinee Manikandan <152697438+Harinee2501@users.noreply.github.com>
2026-05-05 10:46:10 +0530

    [61] [Time Beats: 100.00%] [Memory Beats: 10.95%] - LeetPush

--- Rotate-List.py
+++ Rotate-List.py
@@ -4,21 +4,23 @@
 4#         self.val = val
 5#         self.next = next
 6class Solution(object):
-7    def rotateRight(self, head, k):
-8        temp=head
-9        count=0
-10        while temp:
-11            count+=1
-12            temp=temp.next
-13        if not head or not head.next:
-14            return head
-15        temp=head
-16        for _ in range(k%count):
-17            while temp.next.next:
-18                temp=temp.next
-19            temp.next.next=head
-20            head=temp.next
-21            temp.next=None
-22            temp=head
-23        return head
-24           
+7    #rotating for k times= rotating for k%n times
+8    def rotateRight(self, head, k):
+9        if not head or not head.next:
+10            return head
+11        temp=head
+12        n=1
+13        while temp.next:
+14            temp=temp.next
+15            n+=1
+16        tail=temp
+17        tail.next=head
+18        new_tail=head
+19        k=k%n
+20        steps=n-k-1
+21        for _ in range(steps):
+22            new_tail=new_tail.next
+23        new_head=new_tail.next
+24        new_tail.next=None
+25        return new_head
+26           
